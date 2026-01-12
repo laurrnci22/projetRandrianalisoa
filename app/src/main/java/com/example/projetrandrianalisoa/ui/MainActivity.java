687195078c194933792c78b6543eb13eebcac860
LalaBritta <randrianalisoa.lbl@gmail.com>
2026-01-12 21:10:50 +0100

    init project

---
+++ app/src/main/java/com/example/projetrandrianalisoa/ui/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.projetrandrianalisoa.ui;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.projetrandrianalisoa.R;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        /*try {
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(getAssets().open("qcm01.txt"))
+            );
+            Log.d("TEST", br.readLine());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return true;
+    }
+
+    public void onCancel(View view) {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+}
